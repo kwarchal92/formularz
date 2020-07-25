680eebd962df55f117ac40ac1703b61c9e8482b9
kwarchal <kamilwarchal295@gmail.com>
2020-07-25 17:38:35 +0200

    formularz-dane

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var przycisk = document.getElementById('button');
+let name = document.getElementById('name');
+let surname = document.getElementById('surname');
+let phone = document.getElementById('phone');
+
+
+
+    przycisk.onclick = function dane(){
+
+        name.textContent = document.getElementById('imie').value;
+        console.log(name);
+
+        surname.textContent = document.getElementById('nazwisko').value;
+        console.log(surname);
+
+        phone.textContent = document.getElementById('telefon').value;
+        console.log(phone);
+      
+    }
+
+
+    
